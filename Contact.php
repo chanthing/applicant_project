db73db66e21fc3be110b358277b1d032da909e6c
Channing Benson <chanthing@gmail.com>
2013-10-28 22:46:24 -0600

    Re-committing to get changes to Contact.php

--- Contact.php
+++ Contact.php
@@ -1,7 +1,33 @@
 <?php
 require_once("./AbstractModel.php");
+/*
+ * Class that implements a simple contact, with two mutable fields: name and email.
+ *
+ * Database functionality is provided by the AbstractModel superclass.
+ */
 Class Contact extends AbstractModel
 {
-	protected $_table = "contacts";
-	protected $_pk	  = "id";
+
+    public function __construct() {
+	parent::construct("contacts", ["id" => NULL, "name" => NULL, "email" => NULL], 0);
+    }
+
+    public function get_name() {
+        return $this->_fieldArray["name"];
+    }
+
+    public function set_name($name) {
+        $this->_fieldArray["name"] = $name;
+    }
+
+    public function get_email() {
+        return $this->_fieldArray["email"];
+    }
+
+    public function set_email($email) {
+        $this->_fieldArray["email"] = $email;
+    }
+
+
+
 }
